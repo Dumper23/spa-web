e7cf6a3c6bf7bf1b19726c31a1b10ff56188fb35
Carles Canos <carlescanos@gmail.com>
2024-11-09 00:05:24 +0100

    deleted spa

--- spa-web/src/app/shared/apollo-client.ts
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
-import { environemnt } from '../environment/Environment';
-
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  uri: environemnt.DB_URI,
-  cache: new InMemoryCache(),
-});
-
-export default client;
